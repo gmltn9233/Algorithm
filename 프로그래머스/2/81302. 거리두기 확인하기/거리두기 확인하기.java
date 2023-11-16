434881e705011e683c87a5bc23b63865ab88601e
이희수 <63588364+gmltn9233@users.noreply.github.com>
2023-11-16 12:26:33 +0900

    [level 2] Title: 거리두기 확인하기, Time: 0.11 ms, Memory: 84.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/81302. 거리두기 확인하기/거리두기 확인하기.java
@@ -0,0 +1,43 @@
+class Solution {
+    static int[] dy={-1,0,1,0};
+    static int[] dx={0,1,0,-1};
+    static boolean[][] visited;
+    static int[] answer;
+    
+    public void dfs(int num, int count, int y, int x, String[] places){
+        if(count>2) return;
+        if(count>0&&count<=2&&places[y].charAt(x)=='P') {
+            answer[num]=0;
+            return;
+        }
+        for(int i=0; i<4; i++){
+            int ny=y+dy[i];
+            int nx=x+dx[i];
+            if(ny<0||nx<0||ny>=5||nx>=5) continue;
+            if(visited[ny][nx]||places[ny].charAt(nx)=='X') continue;
+            visited[ny][nx]=true;
+            dfs(num,count+1,ny,nx,places);
+            visited[ny][nx]=false;
+        }
+    }
+    
+    public int[] solution(String[][] places) {
+        answer= new int[places.length];
+        for(int i=0; i<5; i++){
+            answer[i]=1;
+        }
+        for(int i=0; i<places.length; i++){
+            visited = new boolean[5][5];
+            for(int j=0; j<5; j++){
+                for(int k=0; k<5; k++){
+                    if(places[i][j].charAt(k)=='P'){
+                        visited[j][k]=true;
+                        dfs(i,0,j,k,places[i]);
+                        visited[j][j]=false;
+                    }
+                }
+            }
+        }
+        return answer;
+    }
+}
