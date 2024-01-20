c9846a5820e20876a25b1a3ebbe8b5b90c522357
이희수 <63588364+gmltn9233@users.noreply.github.com>
2024-01-20 20:36:42 +0900

    [Gold IV] Title: 알파벳, Time: 420 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.cc
+++ 백준/Gold/1987. 알파벳/알파벳.cc
@@ -1,36 +1,31 @@
 #include<bits/stdc++.h>
-using namespace std;
-const int max_n = 21;
-const int dy[4]={0,1,0,-1};
-const int dx[4]={1,0,-1,0};
-int R,C,visited[30],ret;
+using namespace std; 
+int r,c;
 string s;
-char a[max_n][max_n];
-
-void bfs(int y, int x, int cnt){
+char a[24][24];
+int dy[4]={0,1,0,-1};
+int dx[4]={1,0,-1,0};
+int ret;
+void go(int y,int x,int num,int cnt){
 	ret=max(ret,cnt);
 	for(int i=0; i<4; i++){
 		int ny= y+dy[i];
 		int nx= x+dx[i];
-		if(ny<0||ny>=R||nx<0||nx>=C) continue;
-		if(visited[(int)a[ny][nx]-'A']==0){
-			visited[(int)a[ny][nx]-'A']=1;
-			bfs(ny,nx,cnt+1);
-			visited[(int)a[ny][nx]-'A']=0;
-		}
+		if(ny<0||ny>=r||nx<0||nx>=c) continue;
+		int _next = (1<<(int)a[ny][nx]-'A');
+		if((num&_next)==0) go(ny,nx,num|_next,cnt+1);
 	}
 	return;
 }
-
 int main(){
-	cin>>R>>C;
-	for(int i=0; i<R;i++){
-		for(int j=0; j<C; j++){
-			cin>>a[i][j];
+	cin>>r>>c;
+	for(int i=0; i<r; i++){
+		cin>>s;
+		for(int j=0; j<c; j++){
+			a[i][j]=s[j];
 		}
 	}
-	visited[(int)a[0][0]-'A']=1;
-	bfs(0,0,1);
-	cout<< ret;
-    return 0;
+	go(0,0,1<<(int)a[0][0]-'A',1);
+	cout<<ret<<'\n';
+	return 0;
 }
