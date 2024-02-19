4e0b751304233587c2e97eed33308c0cec1c7740
이희수 <63588364+gmltn9233@users.noreply.github.com>
2024-02-20 01:39:23 +0900

    [Bronze V] Title: 그대로 출력하기, Time: 4 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.cc
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+map<string,int> mp;
+int main(){
+	while(true){
+		string s;
+		getline(cin,s);
+		if(s.empty()){
+			break;
+		}
+		cout<<s<<'\n';
+	}
+return 0;
+}
+
