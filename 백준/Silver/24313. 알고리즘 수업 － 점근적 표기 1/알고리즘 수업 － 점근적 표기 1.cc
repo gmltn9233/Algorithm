790ac78304d26cc6a96c08604aa92d524a5a96e1
이희수 <63588364+gmltn9233@users.noreply.github.com>
2024-03-25 19:15:58 +0900

    [Silver V] Title: 알고리즘 수업 - 점근적 표기 1, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/24313. 알고리즘 수업 － 점근적 표기 1/알고리즘 수업 － 점근적 표기 1.cc
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int a1,a0,c,n0;
+int main(){
+	cin>>a1>>a0;
+	cin>>c;
+	cin>>n0;
+	if(a1*n0+a0<=c*n0&&a1 <= c){
+		cout<< 1;
+	}
+	else{
+		cout<<0;
+	}
+	return 0;
+}
+
