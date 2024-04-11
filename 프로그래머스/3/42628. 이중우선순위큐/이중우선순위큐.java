4150652cfddaa11e63195f463762e0a2c52facdc
이희수 <63588364+gmltn9233@users.noreply.github.com>
2024-04-11 23:46:09 +0900

    [level 3] Title: 이중우선순위큐, Time: 3.30 ms, Memory: 76.7 MB -BaekjoonHub

--- 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
@@ -2,39 +2,31 @@ import java.util.*;
 class Solution {
     public int[] solution(String[] operations) {
         Deque<Integer> dq = new ArrayDeque<>();
-        PriorityQueue<Integer> pq1 = new PriorityQueue<>((o1,o2)->o1-o2);
-        PriorityQueue<Integer> pq2 = new PriorityQueue<>((o1,o2)->o2-o1);
-        List <Integer> answer = new ArrayList<>();
         for(String s : operations){
-            int num = 0;
             if(s.charAt(0)=='I'){
                 if(s.charAt(2)=='-'){
-                    num = -1 * Integer.parseInt(s.substring(3));
+                    dq.add(-1 * Integer.parseInt(s.substring(3)));
                 }
                 else{
-                    num = Integer.parseInt(s.substring(2));
+                    dq.add(Integer.parseInt(s.substring(2)));
                 }
-                dq.add(num);
-                List<Integer> tempList = new ArrayList<>(dq);
-        
-                // ArrayList 정렬
-                Collections.sort(tempList);
-        
-                // 정렬된 요소를 다시 ArrayDeque로 복사
+                List<Integer> dqList = new ArrayList<>(dq);
+                dqList.sort((o1,o2)->o1-o2);
                 dq.clear();
-                dq.addAll(tempList);
+                dq.addAll(dqList);
             }
-            if(s.charAt(0)=='D'){
-                if(s.charAt(2)=='-'&&!dq.isEmpty()){
-                    dq.removeFirst();
-                }
-                else{
-                    if(!dq.isEmpty()){
+            else{
+                if(!dq.isEmpty()){
+                    if(s.charAt(2)=='-'){
+                        dq.removeFirst();
+                    }
+                    else{
                         dq.removeLast();
                     }
                 }
             }
         }
+        List<Integer> answer = new ArrayList<>();
         if(dq.isEmpty()){
             answer.add(0);
             answer.add(0);
