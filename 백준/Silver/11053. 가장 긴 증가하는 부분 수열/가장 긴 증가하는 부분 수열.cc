ba1767f73473c6b2012b1a889f844eee2596e812
이희수 <63588364+gmltn9233@users.noreply.github.com>
2024-05-04 16:16:13 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

---
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.cc
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std; 
+int n;
+int a[1001],cnt[1001],ret;
+int main(){
+	cin>>n;
+	for(int i=0; i<n; i++){
+		cin>>a[i];
+	}
+	for(int i=0; i<n; i++){
+		int maxValue = 0;
+		for(int j=0; j< i; j++){
+			if(a[j]<a[i] && maxValue < cnt[j]) maxValue = cnt[j];
+		}
+		cnt[i] = maxValue + 1;
+		ret = max(ret,cnt[i]);
+	}
+	cout<<ret;
+	return 0;
+}
