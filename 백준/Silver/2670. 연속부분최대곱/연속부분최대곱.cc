a0a91a8cec7ea29c76eed801408f497507aa9702
이희수 <63588364+gmltn9233@users.noreply.github.com>
2024-05-10 17:22:30 +0900

    [Silver IV] Title: 연속부분최대곱, Time: 4 ms, Memory: 2100 KB -BaekjoonHub

--- 백준/Silver/2670. 연속부분최대곱/연속부분최대곱.cc
+++ 백준/Silver/2670. 연속부분최대곱/연속부분최대곱.cc
@@ -1,18 +1,16 @@
 #include<bits/stdc++.h> 
 using namespace std;
 int n;
-double a[10004],b[10004],ret;
+double a[10004],sum=1,ret;
 int main() {
 	cin>>n;
 	for(int i=0; i<n; i++){
 		cin>>a[i];
-	}
-	for(int i=0; i<n; i++){
-		double sum = 1;
-		for(int j=i; j<n; j++){
-			sum *= a[j];
-			ret = max(sum,ret);
+		sum *= a[i];
+		if(sum < a[i]){
+			sum = a[i];
 		}
+		ret = max(ret,sum);
 	}
 	printf("%.3lf",ret);
 	return 0;
