0fe059676b74297f3930ad1ee27936587f156b32
이희수 <63588364+gmltn9233@users.noreply.github.com>
2024-05-22 00:49:19 +0900

    [Gold V] Title: 파이프 옮기기 1, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.cc
+++ 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.cc
@@ -20,7 +20,7 @@ int main() {
 	}
 	dp[1][2][0]=1;
 	for(int i=1; i<=n; i++){
-		for(int j=1; j<=n; j++){
+		for(int j=2; j<=n; j++){
 			if(check(i, j + 1, 0))dp[i][j + 1][0] += dp[i][j][0];
             if(check(i + 1, j + 1, 1))dp[i + 1][j + 1][1] += dp[i][j][0]; 
 
