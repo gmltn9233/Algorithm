98921756b5868afcf18a140b1fa7b0c74f729f3c
이희수 <63588364+gmltn9233@users.noreply.github.com>
2024-05-21 20:58:41 +0900

    [Gold I] Title: 외판원 순회, Time: 28 ms, Memory: 6116 KB -BaekjoonHub

--- 백준/Gold/2098. 외판원 순회/외판원 순회.cc
+++ 백준/Gold/2098. 외판원 순회/외판원 순회.cc
@@ -1,20 +1,17 @@
 #include<bits/stdc++.h> 
 using namespace std;
-int n, dp[16][1<<16], dist[16][16];
-
+int n,dist[16][16],dp[16][1<<16];
 int tsp(int here, int visited){
-	// 모든 지점을 방문했다면 
-	if(visited==(1<<n)-1){
-		//돌아오는 경로가 없다면 INF 반환 (배제) 
+	if(visited == (1<<n)-1){
 		return dist[here][0] ? dist[here][0] : 987654321;
 	}
-	int &ret = dp[here][visited];
+	int &ret= dp[here][visited];
 	if(ret != -1) return ret;
 	ret = 987654321;
 	for(int i=0; i<n; i++){
-		if(visited & (1<<i)) continue; //n번 idx 방문한 적 있으면 
-		if(dist[here][i]==0) continue; //시작점이면 
-		ret = min(ret,tsp(i,visited|(1<<i))+dist[here][i]);
+		if(visited & (1<<i)) continue;
+		if(dist[here][i]==0) continue;
+		ret = min(ret, tsp(i,visited | (1<<i)) + dist[here][i]);
 	}
 	return ret;
 }
@@ -24,9 +21,8 @@ int main() {
 		for(int j=0; j<n; j++){
 			cin>>dist[i][j];
 		}
-	} 
+	}
 	memset(dp,-1,sizeof(dp));
-	cout<<tsp(0,1); 
+	cout<<tsp(0,1);
 	return 0;
 }
-
