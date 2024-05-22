cc0b1e459c962e51b93a924ba56bd2e25c3ba05e
이희수 <63588364+gmltn9233@users.noreply.github.com>
2024-05-22 19:16:59 +0900

    [Gold II] Title: 게임, Time: 0 ms, Memory: 2048 KB -BaekjoonHub

---
+++ 백준/Gold/1103. 게임/게임.cc
@@ -0,0 +1,41 @@
+#include<bits/stdc++.h> 
+using namespace std;
+int n,m,d[51][51];
+char a[51][51];
+int dy[4]={-1,0,1,0};
+int dx[4]={0,1,0,-1};
+int ret;
+int visited[51][51];
+int go(int y, int x){
+	//cout<<"y: "<<y<<"x: "<<x<<"cnt: "<<cnt<<"\n";
+	if(y<0 || y>=n || x<0 || x>=m ||a[y][x]=='H'){
+		return 0;
+	}
+	if(visited[y][x]){
+		cout<<-1;
+		exit(0);
+	}
+	int &ret =d[y][x];
+	if(ret) return ret;
+	visited[y][x]=1;
+	int num = a[y][x]-'0';
+	for(int i=0; i<4; i++){
+		int ny = y+dy[i]*num;
+		int nx = x+dx[i]*num;
+		ret = max(ret,go(ny,nx)+1);
+	}
+	visited[y][x]=0;
+	return ret;
+}
+int main() {
+	cin>>n>>m;
+	for(int i=0; i<n; i++){
+		string s;
+		cin>>s;
+		for(int j=0; j<m; j++){
+			a[i][j]=s[j];
+		}
+	}
+	cout<<go(0,0);
+	return 0;
+}
