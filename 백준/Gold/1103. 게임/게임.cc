7c0938bf2e68068452dffc18868df3e6194b48ae
이희수 <63588364+gmltn9233@users.noreply.github.com>
2024-05-23 21:44:26 +0900

    Update 게임.cc

--- 백준/Gold/1103. 게임/게임.cc
+++ 백준/Gold/1103. 게임/게임.cc
@@ -7,24 +7,26 @@ int dx[4]={0,1,0,-1};
 int ret;
 int visited[51][51];
 int go(int y, int x){
-	//cout<<"y: "<<y<<"x: "<<x<<"cnt: "<<cnt<<"\n";
 	if(y<0 || y>=n || x<0 || x>=m ||a[y][x]=='H'){
 		return 0;
 	}
 	if(visited[y][x]){
 		cout<<-1;
+		// 재방문시 바로 종료
 		exit(0);
 	}
+	//메모이제이션
 	int &ret =d[y][x];
 	if(ret) return ret;
-	visited[y][x]=1;
+	
+	visited[y][x]=1; //방문 표시
 	int num = a[y][x]-'0';
 	for(int i=0; i<4; i++){
 		int ny = y+dy[i]*num;
 		int nx = x+dx[i]*num;
 		ret = max(ret,go(ny,nx)+1);
 	}
-	visited[y][x]=0;
+	visited[y][x]=0; //방문 해제
 	return ret;
 }
 int main() {
