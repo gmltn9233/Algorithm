7776c14829de608277a3848f592b4422d2904584
이희수 <63588364+gmltn9233@users.noreply.github.com>
2024-05-27 00:58:39 +0900

    [Gold V] Title: 알약, Time: 28 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Gold/4811. 알약/알약.cc
+++ 백준/Gold/4811. 알약/알약.cc
@@ -3,13 +3,16 @@ using namespace std;
 typedef long long ll;
 ll n;
 ll dp[31][31];
-ll go(int w, int h){
-	if(w == 0 && h==0) return 1;
 
+ll go(int w, int h){
+	if(w==0 && h==0) return 1;
+	
 	ll &ret = dp[w][h];
 	if(ret) return ret;
-	if(w>0) ret += go(w-1,h+1);
+	
+	if(w>0) ret += go(w-1,h+1); 
 	if(h>0) ret += go(w,h-1);
+	
 	return ret;
 }
 int main() {
