80572c5efbc556b694542ec9adac04eece0b9f45
이희수 <63588364+gmltn9233@users.noreply.github.com>
2024-06-01 22:51:30 +0900

    [Gold V] Title: 평범한 배낭, Time: 8 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.cc
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.cc
@@ -1,11 +1,13 @@
 #include<bits/stdc++.h> 
 using namespace std;
 int n,k,w,v;
-int dp[100004];
+int dp[100001];
 int main() {
 	cin>>n>>k;
 	for(int i=0; i<n; i++){
 		cin>>w>>v;
+        // 유한한 경우 왼쪽 탐색
+        // 무한한 경우 오른쪽 탐색
 		for(int j=k; j>=w; j--){
 			dp[j]= max(dp[j],dp[j-w]+v);
 		}
