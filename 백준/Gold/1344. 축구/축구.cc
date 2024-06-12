5998653b8367cac1232687f80245b7a6815a52d8
이희수 <63588364+gmltn9233@users.noreply.github.com>
2024-06-12 11:45:21 +0900

    [Gold IV] Title: 축구, Time: 0 ms, Memory: 2076 KB -BaekjoonHub

--- 백준/Gold/1344. 축구/축구.cc
+++ 백준/Gold/1344. 축구/축구.cc
@@ -1,13 +1,14 @@
 #include<bits/stdc++.h> 
 using namespace std;
 double a,b;
-bool isP[20];
-double dp[20][20][20];
+bool isP[19];
+double dp[19][19][19];
 double go(int idx, int x, int y){
 	if(idx == 18) return (isP[x]||isP[y]) ? 1.0 : 0.0;
 	
 	double &ret = dp[idx][y][x];
 	if(ret>-1) return ret;
+	// double은 == 비교 금지 
 	ret = 0.0;
 	ret += go(idx+1,x,y)*(1-a)*(1-b);
 	ret += go(idx+1,x+1,y)*(a)*(1-b);
