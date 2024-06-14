f0180f6f15d98df5533846a2e4ac59062592dfbd
이희수 <63588364+gmltn9233@users.noreply.github.com>
2024-06-14 11:45:31 +0900

    [Gold I] Title: 팰린드롬 분할, Time: 64 ms, Memory: 26468 KB -BaekjoonHub

--- 백준/Gold/1509. 팰린드롬 분할/팰린드롬 분할.cc
+++ 백준/Gold/1509. 팰린드롬 분할/팰린드롬 분할.cc
@@ -3,27 +3,29 @@ using namespace std;
 const int INF = 987654321;
 string s;
 int dp[2501][2501],dp2[2501];
-int go(int here){
-	if(here == s.size()) return 0;
+int go(int idx){
+	if(idx == s.size()) return 0;
 	
-	int &ret = dp2[here];
+	int &ret = dp2[idx];
 	if(ret != INF) return ret;
-	for(int i=1; i+here <= s.size(); i++){
-		if(dp[here][i]) ret = min(ret,go(here+i)+1);
+	
+	for(int i=1; idx+i<=s.size(); i++){
+		if(dp[idx][i]){
+			ret = min(ret,go(idx+i)+1);
+		}
 	}
+	
 	return ret;
 }
 int main(){
 	cin>>s;
 	for(int i=0; i<s.size(); i++) dp[i][1] = 1;
 	for(int i=0; i<s.size()-1; i++){
-		if(s[i]==s[i+1]){
-			dp[i][2]=1;
-		}
+		if(s[i]==s[i+1]) dp[i][2] = 1;
 	}
 	for(int i=3; i<=s.size(); i++){
-		for(int j=0; j+i <= s.size(); j++){
-			if(s[j]==s[j+i-1] && dp[j+1][i-2]) dp[j][i] = 1;
+		for(int j=0; j+i<=s.size(); j++){
+			if(s[j]==s[j+i-1] && dp[j+1][i-2]) dp[j][i]=1;
 		}
 	}
 	fill(dp2,dp2+2501,INF);
