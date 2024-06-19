fcdf070a911912e0c3620ca7d0758b95de95a727
이희수 <63588364+gmltn9233@users.noreply.github.com>
2024-06-19 16:30:08 +0900

    [Gold V] Title: 1학년, Time: 0 ms, Memory: 2036 KB -BaekjoonHub

--- 백준/Gold/5557. 1학년/1학년.cc
+++ 백준/Gold/5557. 1학년/1학년.cc
@@ -1,24 +1,30 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h> 
 using namespace std;
 typedef long long ll;
-ll n, a[104], dp[104][21];
-ll go(int idx, int sum){
-    if(sum < 0 ||sum >20) return 0;
-    ll &ret = dp[idx][sum];
-    if(ret) return ret;
-    if(idx == n - 2){
-        if(sum == a[n - 1]) return 1;
-        return 0;
-    } 
-    ret += go(idx + 1, sum + a[idx + 1]);
-    ret += go(idx + 1, sum - a[idx + 1]);
-    return ret;
+int n;
+int a[101];
+ll dp[101][21];
+ll go(int num,int idx){
+	if(num<0 || num>20) return 0;
+	if(idx==n-2){
+		if(num==a[n-1]) return 1;
+		return 0;
+	}
+	
+	ll &ret = dp[idx][num];
+	if(ret) return ret;
+	
+	ret = 0;
+	ret += go(num+a[idx+1],idx+1);
+	ret += go(num-a[idx+1],idx+1);
+	
+	return ret;
 }
 int main(){
-    cin >> n;
-    for(int i = 0; i < n; i++){
-        cin >> a[i];
-    } 
-    cout << go(0, a[0]) << "\n";
-    return 0;
+	cin>>n;
+	for(int i=0; i<n; i++){
+		cin>>a[i];
+	}
+	cout<<go(a[0],0);
+	return 0;
 }
