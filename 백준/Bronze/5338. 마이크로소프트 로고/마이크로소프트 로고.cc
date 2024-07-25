e769e4ee253c011b24d5b5171551dd1916594f60
이희수 <63588364+gmltn9233@users.noreply.github.com>
2024-07-25 23:48:49 +0900

    [Bronze V] Title: 마이크로소프트 로고, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/5338. 마이크로소프트 로고/마이크로소프트 로고.cc
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h> 
+using namespace std;
+string s;
+int ret;
+int main(){
+	cout<<"       _.-;;-._\n";
+	cout<<"'-..-'|   ||   |\n";
+	cout<<"'-..-'|_.-;;-._|\n";
+	cout<<"'-..-'|   ||   |\n";
+	cout<<"'-..-'|_.-''-._|\n";
+}
