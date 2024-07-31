2dbc46293700abbd6ae2dbed8670c9c8e2cbcdb5
이희수 <63588364+gmltn9233@users.noreply.github.com>
2024-07-31 17:18:55 +0900

    [Platinum IV] Title: 영화 수집, Time: 196 ms, Memory: 7556 KB -BaekjoonHub

--- 백준/Platinum/3653. 영화 수집/영화 수집.cc
+++ 백준/Platinum/3653. 영화 수집/영화 수집.cc
@@ -1,5 +1,5 @@
-#include<bits/stdc++.h>
-using namespace std;    
+#include<bits/stdc++.h> 
+using namespace std;
 int t, n, m, tree[200004], temp; 
 map<int, int> mp; 
 void update(int idx, int value){
@@ -17,25 +17,27 @@ int sum(int idx){
     return ret; 
 }
 int main(){
-    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-    cin >> t; 
-    while(t--){
-        cin >> n >> m; 
-        memset(tree, 0, sizeof(tree));
-        mp.clear();
-        int update_idx = 100001; 
-        for(int i = 1; i <= n; i++){
-            update(i + update_idx, 1);
-            mp[i] = i + update_idx;  
-        } 
-        for(int i = 0; i < m; i++){
-            cin >> temp;  
-            int idx = mp[temp];   
-            cout << sum(idx) - 1 << " ";
-            update(idx, -1);  
-            update(--update_idx, 1);   
-            mp[temp] = update_idx;  
-        }
-        cout << "\n";
-    } 
+	ios_base::sync_with_stdio(false); 
+	cin.tie(NULL); 
+	cout.tie(NULL);
+	cin>>t;
+	while(t--){
+		cin>>n>>m;
+		memset(tree,0,sizeof(tree));
+		mp.clear();
+		int update_idx = 100001;
+		for(int i=1; i<=n; i++){
+			update(i+update_idx, 1);
+			mp[i] = i+update_idx;
+		}
+		for(int i=0; i<m; i++){
+			cin>>temp;
+			int idx = mp[temp];
+			cout<< sum(idx)-1<<" ";
+			update(idx,-1);
+			update(--update_idx, 1);
+			mp[temp] = update_idx;
+		}
+		cout<<"\n";
+	}
 }
