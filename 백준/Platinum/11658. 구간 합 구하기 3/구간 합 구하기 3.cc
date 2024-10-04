3952572d3d93cddf99fae182a95e6418b6c93376
이희수 <63588364+gmltn9233@users.noreply.github.com>
2024-10-04 16:43:55 +0900

    [Platinum IV] Title: 구간 합 구하기 3, Time: 156 ms, Memory: 10308 KB -BaekjoonHub

--- 백준/Platinum/11658. 구간 합 구하기 3/구간 합 구하기 3.cc
+++ 백준/Platinum/11658. 구간 합 구하기 3/구간 합 구하기 3.cc
@@ -1,52 +1,53 @@
-#include<cstdio>
-#define max_n 1025
-int n, m;
-int a[max_n][max_n], bit[max_n][max_n];
-void update(int y, int x, int value){
-    while(y <= n){
-        int j = x;
-        while(j <= n){
-            bit[y][j] += value;
-            j += (j & -j);
-        }
-        y += (y & -y);
-    }
-    return;
+#include <bits/stdc++.h>
+typedef long long ll;
+using namespace std;
+int n,m,w,a,b,c,d,C;
+int arr[1030][1030], tree[1030][1030];
+void update(int y,int x, int value){
+	while(y<=n){
+		int j=x;
+		while(j<=n){
+			tree[y][j] += value;
+			j += (j & -j);
+		}
+		y += (y&-y);
+	}
+	return;
 }
-int sum(int y, int x) {
-    int s = 0;
-    while(y > 0){
-        int j = x;
-        while(j > 0){
-            s += bit[y][j];
-            j -= (j & -j);
-        }
-        y -= (y&-y);
-    }
-    return s;
+int sum(int y, int x){
+	int num = 0;
+	while(y>0){
+		int j = x;
+		while(j>0){
+			num+= tree[y][j];
+			j-=(j&-j);
+		}
+		y-=(y&-y);
+	}
+	return num;
 }
 int main() {
-    scanf("%d%d", &n, &m);
-    for (int i = 1; i <= n; i++){
-        for (int j = 1; j <= n; j++){
-            scanf("%d", &a[i][j]);
-            update(i, j, a[i][j]);
-        }
-    }
-
-    for (int i = 0; i < m; i++) {
-        int type;
-        scanf("%d", &type);
-        if (type) {
-            int y1, x1, y2, x2;
-            scanf("%d%d%d%d",&y1, &x1, &y2, &x2);
-            printf("%d\n", sum(y2, x2) - sum(y1 - 1, x2) - sum(y2, x1 - 1) + sum(y1 - 1, x1 - 1));
-        }
-        else{
-            int y, x, value;
-            scanf("%d%d%d",&y, &x, &value);
-            update(y, x, value - a[y][x]), a[y][x] = value;
-        }
-    }
-    return 0;
+    cin.tie(NULL); //입출력 묶음 해제
+    ios_base::sync_with_stdio(false);
+	cin>>n>>m;
+	for(int i=1; i<=n; i++){
+		for(int j=1; j<=n; j++){
+			cin>>arr[i][j];
+			update(i,j,arr[i][j]);
+		}
+	}
+	while(m--){
+		cin>>w;
+		if(w==0){
+			cin>>a>>b>>C;
+			update(a,b,C-arr[a][b]);
+			arr[a][b]=C;
+		}
+		else{
+			cin>>a>>b>>c>>d;
+			cout<<sum(c,d)-sum(a-1,d)-sum(c,b-1)+sum(a-1,b-1)<<"\n";
+		}
+	}
+	return 0;
 }
+ 
