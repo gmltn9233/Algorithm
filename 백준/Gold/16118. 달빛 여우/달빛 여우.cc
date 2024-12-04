9a12587d749100c751f37210f62788ef6e6e12f9
이희수 <63588364+gmltn9233@users.noreply.github.com>
2024-12-04 14:31:22 +0900

    [Gold I] Title: 달빛 여우, Time: 140 ms, Memory: 10300 KB -BaekjoonHub

--- 백준/Gold/16118. 달빛 여우/달빛 여우.cc
+++ 백준/Gold/16118. 달빛 여우/달빛 여우.cc
@@ -3,12 +3,12 @@ using namespace std;
 const int INF = 987654321;
 int n,m,a,b,d,ret;
 vector<pair<int,int>> route[4001];
-int dist[4001],dist_wolf[4001][2];
+int dist[4001], dist2[4001][2];
 void dikstra(){
-	fill(dist,dist+4001,INF);
-	priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> pq;
-	dist[1] = 0;
+	priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> pq;
 	pq.push({0,1});
+	fill(dist,dist+4001,INF);
+	dist[1]=0;
 	while(pq.size()){
 		int here = pq.top().second;
 		int here_dist = pq.top().first;
@@ -24,40 +24,49 @@ void dikstra(){
 		}
 	}
 }
+
 void dikstra2(){
-	fill(&dist_wolf[0][0], &dist_wolf[0][0]+4001*2 , INF);
-	priority_queue<tuple<int,int,bool>,vector<tuple<int,int,bool>>, greater<tuple<int,int,bool>>> pq;
-	dist_wolf[1][0] = 0;
+	priority_queue<tuple<int,int,bool>, vector<tuple<int,int,bool>>, greater<tuple<int,int,bool>>> pq;
 	pq.push(make_tuple(0,1,0));
+	fill(&dist2[0][0], &dist2[0][0] + 4001 * 2 , INF);
+	dist2[1][0]=0;
 	while(pq.size()){
-		int d,here,cnt;
-		tie(d,here,cnt) = pq.top();
+		int here,here_dist,flag;
+		tie(here_dist,here,flag) = pq.top();
 		pq.pop();
-		if(dist_wolf[here][cnt] != d) continue;
+		if(dist2[here][flag] != here_dist) continue;
 		for(pair<int,int> there : route[here]){
 			int next = there.second;
-			int next_dist = !cnt ? there.first/2 : there.first*2;
-			if(dist_wolf[next][!cnt] > dist_wolf[here][cnt] + next_dist){
-				dist_wolf[next][!cnt] = dist_wolf[here][cnt] + next_dist;
-				pq.push(make_tuple(dist_wolf[next][!cnt], next, !cnt));
+			int next_dist = flag ? there.first*2 : there.first/2;
+			if(dist2[next][!flag] > dist2[here][flag] + next_dist){
+				dist2[next][!flag] = dist2[here][flag] + next_dist;
+				pq.push(make_tuple(dist2[next][!flag],next,!flag));
 			}
-		}
+		}  
 	}
 }
 int main() {
-	cin>>n>>m;
+	cin>>n>>m; 
 	for(int i=0; i<m; i++){
 		cin>>a>>b>>d;
 		route[a].push_back({d*2,b});
 		route[b].push_back({d*2,a});
 	}
-	dikstra();  
-    dikstra2();  
-    for(int i=2; i<=n; i++){
-    	int d = min(dist_wolf[i][0], dist_wolf[i][1]);
-    	if(d>dist[i]) ret ++;
+	dikstra();
+	dikstra2();
+	for(int i=2; i<=n; i++){
+		int fox = dist[i];
+		int wolf = min(dist2[i][0],dist2[i][1]);
+		if(fox<wolf) ++ret;
 	}
 	cout<<ret;
 	return 0;
 }
 
+
+// 14:00 start
+// n개의 그루터기, m개의 오솔길 
+// 여우와 늑대 모두 1번에 살고 있음 
+// 여우 일정한 속도 
+// 늑대 2배속도 1/2배 속도 반복 
+// 여우가 늑대보다 먼저 도착하는 그루터기의 수 ? 
