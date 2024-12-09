a0ae389bb675fecafe0421e359f44cea84d59db8
이희수 <63588364+gmltn9233@users.noreply.github.com>
2024-12-09 14:17:51 +0900

    [Platinum V] Title: 오민식의 고민, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Platinum/1219. 오민식의 고민/오민식의 고민.cc
+++ 백준/Platinum/1219. 오민식의 고민/오민식의 고민.cc
@@ -1,23 +1,33 @@
 #include <bits/stdc++.h>
 using namespace std;
 const int INF = 987654321;
-int n,S,s,E,e,m,p; 
-bool flag;
+int n,m,S,E,s,e,p;
 vector<pair<int,int>> route[51];
-long long price[51], dist[51];
-int visited[104];
-void bellmanFord(){
+long long dist[51],price[51],visited[51];
+bool flag;
+int main() {
+	cin>>n>>S>>E>>m; 
+	for(int i=0; i<m; i++){
+		cin>>s>>e>>p;
+		route[s].push_back({e,p});
+	}
+	for(int i=0; i<n; i++){
+		cin>>price[i];
+	}
+	
+	queue<int> q;
 	fill(dist,dist+51, -INF);
 	dist[S] = price[S];
-	queue<int> q;
 	for(int i=0; i<n; i++){
 		for(int here=0; here<n; here++){
-			for(auto there: route[here]){
-				int next = there.first;
-				int next_dist = there.second;
-				if(dist[here] != -INF && dist[next] < dist[here] + price[next] - next_dist){
-					dist[next] = dist[here] + price[next] - next_dist;
-					if(i==n-1) q.push(here);
+			for(auto there : route[here]){
+				int now = there.first;
+				int now_dist = there.second;
+				if(dist[here] != -INF && dist[now] < dist[here] + price[now] - now_dist){
+					dist[now] = dist[here] + price[now] - now_dist;
+					if(i==n-1){
+						q.push(here);
+					}
 				}
 			}
 		}
@@ -28,7 +38,8 @@ void bellmanFord(){
 		q.pop();
 		for(auto there : route[here]){
 			if(there.first == E){
-				flag = true; break;
+				flag = true;
+				break;
 			}
 			if(!visited[there.first]){
 				visited[there.first] = 1;
@@ -37,24 +48,14 @@ void bellmanFord(){
 		}
 		if(flag) break;
 	}
+	
 	if(dist[E] == -INF) cout<<"gg";
-	else if (flag) cout<<"Gee";
+	else if(flag) cout<<"Gee";
 	else cout<<dist[E];
-}
-int main() {
-	cin>>n>>S>>E>>m;
-	for(int i=0; i<m; i++){
-		cin>>s>>e>>p;
-		route[s].push_back({e,p});
-	}
-	for(int i=0; i<n; i++){
-		cin>>price[i];
-	}
-	bellmanFord();
+	
 	return 0;
 }
 
-// 최대한 많이 팔아서 최대 이윤
-// n개의 도시 (0번~n-1번)
-// a 시작 b 끝
-// m 개의 교통수단 
+// n개의 도시 (0~n-1)
+// m개의 교통 수단
+ 
