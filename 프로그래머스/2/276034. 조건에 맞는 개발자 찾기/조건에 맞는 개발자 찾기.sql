1cd7dd64ab834f82d5e6355a08414ff80fec4296
이희수 <63588364+gmltn9233@users.noreply.github.com>
2025-01-12 22:26:59 +0900

    [level 2] Title: 조건에 맞는 개발자 찾기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/2/276034. 조건에 맞는 개발자 찾기/조건에 맞는 개발자 찾기.sql
@@ -0,0 +1,12 @@
+SELECT
+    DISTINCT id,
+    email,
+    first_name,
+    last_name
+FROM
+    developers d
+    JOIN skillcodes s
+    ON s.name IN ('C#', 'Python')
+    AND d.skill_code & s.code = s.code
+ORDER BY
+    1
