cf0f2efe70a147a2172e21874a7596d368b5e2d9
이희수 <hslee9234@naver.com>
2025-04-18 17:19:52 +0900

    [level 3] Title: 베스트앨범, Time: 0.05 ms, Memory: 3.59 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.cpp
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.cpp
@@ -1,42 +1,46 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int n;
+map<string, int> genre;
+map<string, vector<pair<int,int>>> music;
+
+bool cmp(pair<int,int> a, pair<int,int> b){
+    if(a.first==b.first) return a<b; 
+    else return a.first >b.first;
+}
+
 vector<int> solution(vector<string> genres, vector<int> plays) {
-    map<string, int> genreMap; // 장르별 총 재생 수
-    map<string, vector<pair<int, int>>> idxMap; // 장르별 (인덱스, 재생 수)
     vector<int> answer;
-
-    // 데이터 입력
-    for (int i = 0; i < genres.size(); i++) {
-        genreMap[genres[i]] += plays[i];
-        idxMap[genres[i]].push_back({i, plays[i]});
+    int n = genres.size();
+    for (int i =0; i<n; i++){
+        genre[genres[i]]+= plays[i];
+        music[genres[i]].push_back(make_pair(plays[i],i));
     }
-
-    // 장르를 재생 수 기준으로 내림차순 정렬
-    vector<pair<int, string>> sortedGenres;
-    for (auto &entry : genreMap) {
-        sortedGenres.push_back({entry.second, entry.first});
+    
+    // genre 정렬
+    vector<pair<int,string>> sorted_genre;
+    for(auto at : genre){
+        sorted_genre.push_back({at.second, at.first});
     }
-    sort(sortedGenres.rbegin(), sortedGenres.rend()); // 내림차순 정렬
-
-    // 각 장르별로 곡을 정렬하고 최대 2개를 선택
-    for (auto &genre : sortedGenres) {
-        string genreName = genre.second;
-        auto &songs = idxMap[genreName];
-
-        // 곡을 재생 수가 높은 순으로, 재생 수가 같으면 인덱스가 낮은 순으로 정렬
-        sort(songs.begin(), songs.end(), [](pair<int, int> &a, pair<int, int> &b) {
-            if (a.second == b.second) {
-                return a.first < b.first; // 재생 수가 같으면 인덱스가 낮은 순
-            }
-            return a.second > b.second; // 재생 수가 높은 순
-        });
-
-        // 최대 두 곡 추가
-        for (int i = 0; i < min(2, (int)songs.size()); i++) {
-            answer.push_back(songs[i].first);
+    
+    sort(sorted_genre.begin(),sorted_genre.end());
+    reverse(sorted_genre.begin(),sorted_genre.end());
+    
+    
+    // 장르별 노래 정렬
+    for(int i=0; i<sorted_genre.size(); i++){
+        vector<pair<int,int>> v = music[sorted_genre[i].second];
+        sort(v.begin(), v.end(), cmp);
+        answer.push_back(v[0].second);
+        if(v.size()>=2){
+            answer.push_back(v[1].second);
         }
     }
-
+    
+    
     return answer;
 }
+// 1. 노래 재생수 합이 큰 장르 먼저 수록
+// 2. 장르 내에서 많이 재생된 노래 먼저 수록
+// 3. 재생 횟수가 같다면 고유번호가 낮은 노래 먼저 수록
