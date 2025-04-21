e7e081864f31a4f95aead61a5fdc686f7eebcf33
이희수 <hslee9234@naver.com>
2025-04-21 20:56:01 +0900

    [level 2] Title: 구명보트, Time: 1.99 ms, Memory: 4.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/42885. 구명보트/구명보트.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int solution(vector<int> people, int limit) {
+    int answer = 0;
+    sort(people.begin(),people.end());
+    int l=0, r=people.size()-1;
+    while(l<=r){
+        if(people[r] + people[l] <= limit){
+            l++;
+            r--;
+            ++answer;
+        }
+        else{
+            r--;
+            ++answer;
+        }
+    }
+    
+    return answer;
+}
