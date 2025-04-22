af62bc80d4ae6f75bf6990d588e433f63be98d5d
이희수 <hslee9234@naver.com>
2025-04-22 09:47:59 +0900

    [Bronze II] Title: 진법 변환, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/2745. 진법 변환/진법 변환.cc
+++ 백준/Bronze/2745. 진법 변환/진법 변환.cc
@@ -1,19 +1,36 @@
-#include<bits/stdc++.h>
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <bits/stdc++.h>
 using namespace std;
+int b;
 string s;
-int a[37];
-int n,ret;
-int main(){
-	cin>>s>>n;
-	reverse(s.begin(),s.end());
-	for(int i=0; i<s.size();i++){
-		if(s[i]>='A'&&s[i]<='Z'){
-			ret+=pow(n,i)*(s[i]-'A'+10);
-		}
-		else{
-			ret+=pow(n,i)*(s[i]-'0');
-		}
-	}
-	cout<<ret;
-	return 0;
+long long go(int n, int b){
+    long long ret = 1;
+    for(int i=0; i<n-1; i++){
+        ret *= b;
+    }
+    return ret;
+}
+int main()
+{
+    cin>>s>>b;
+    long long ret = 0;
+    for(int i=s.size()-1; i>=0; i--){
+        if(s[i]>='0' && s[i]<='9'){
+            ret += (s[i]-'0')*go(s.size()-i,b);
+        }
+        else{
+            ret += (s[i]-'A'+10)*go(s.size()-i,b);
+        }
+        
+    }
+    
+    cout<<ret;
+    return 0;
 }
