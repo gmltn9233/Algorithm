be0fced11017e06b18eb90d9db12594f31ca847b
이희수 <hslee9234@naver.com>
2025-04-24 20:02:39 +0900

    [level 3] Title: 정수 삼각형, Time: 0.38 ms, Memory: 8.22 MB -BaekjoonHub

--- 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.cpp
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.cpp
@@ -1,20 +1,13 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 
 using namespace std;
-
+int dp[501][501];
 int solution(vector<vector<int>> triangle) {
     int n = triangle.size();
-    
-    // DP 테이블을 triangle 크기에 맞게 할당
-    vector<vector<int>> dp = triangle;
-
-    // 아래에서부터 위로 올라가면서 각 위치의 최대 경로를 계산
-    for (int i = n - 2; i >= 0; --i) {
-        for (int j = 0; j < triangle[i].size(); ++j) {
-            dp[i][j] = max(dp[i+1][j], dp[i+1][j+1]) + triangle[i][j];
+    for(int i=n-2; i>=0; i--){
+        for(int j=0; j<=i; j++){
+            triangle[i][j] = triangle[i][j] + max(triangle[i+1][j],triangle[i+1][j+1]);
         }
     }
-
-    // dp[0][0]에 최종적으로 최대값이 저장됨
-    return dp[0][0];
+    return triangle[0][0];
 }
