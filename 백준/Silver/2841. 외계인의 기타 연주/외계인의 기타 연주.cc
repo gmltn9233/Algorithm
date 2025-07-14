464ce2783eb5a2912dec17b173922783e7687e59
이희수 <hslee9234@naver.com>
2025-07-14 20:21:29 +0900

    [Silver I] Title: 외계인의 기타 연주, Time: 312 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/2841. 외계인의 기타 연주/외계인의 기타 연주.cc
+++ 백준/Silver/2841. 외계인의 기타 연주/외계인의 기타 연주.cc
@@ -9,25 +9,36 @@ Write your code in this editor and press "Run" button to compile and execute it.
 #include <bits/stdc++.h>
 using namespace std;
 int n,p,a,b,ret;
-priority_queue<int,vector<int>,less<int>> pq[7];
+stack<int> st[7];
 int main()
 {
     cin>>n>>p;
     for(int i=0; i<n; i++){
         cin>>a>>b;
-        while(pq[a].size()){
-            int top = pq[a].top();
+        // pq[a]가 빈 큐가 아닐 때
+        while(st[a].size()){
+            int top = st[a].top();
+            // 우선순위 큐의 top(최댓값) 이 
+            // 현재 값 보다 크다면 손가락을 뗌
             if(top>b){
-                pq[a].pop();
+                st[a].pop();
                 ++ret;
-            }else{
+            }
+            // 현재 값 보다 작다면 break;
+            else{
                 break;
             }
         }
-        if(pq[a].size() && pq[a].top()==b) continue;
-        pq[a].push(b);
+        // 최댓값이 같은 값이라면 손가락 안떼도 됨
+        if(st[a].size() && st[a].top()==b) continue;
+        // 아닐 경우 새 손가락 추가
+        st[a].push(b);
         ++ret;
     }
     cout<<ret;
     return 0;
 }
+
+
+// n -> 탐색 요청 수
+// p -> 
