7b810230d13a0e7340caff7b08a4182a1680e1ba
이희수 <hslee9234@naver.com>
2025-08-20 16:23:47 +0900

    [Silver V] Title: 숫자 카드, Time: 404 ms, Memory: 28176 KB -BaekjoonHub

---
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.cc
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+int n,m;
+long long num;
+set<long long> a;
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    cin>>n;
+    for(int i=0; i<n; i++){
+        cin>>num;
+        a.insert(num);
+    }
+    cin>>m;
+    string s = "";
+    for(int i=0; i<m; i++){
+        cin>>num;
+        if(a.find(num)!=a.end()) s+="1";
+        else s+="0";
+        s+=" ";
+    }
+    cout<<s;
+    return 0;
+}
