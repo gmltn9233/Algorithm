2d857c178fb3356670682e90855eb0e5dd620d9e
이희수 <hslee9234@naver.com>
2025-08-25 18:16:10 +0900

    [Platinum V] Title: 가장 긴 증가하는 부분 수열 5, Time: 220 ms, Memory: 35052 KB -BaekjoonHub

--- 백준/Platinum/14003. 가장 긴 증가하는 부분 수열 5/가장 긴 증가하는 부분 수열 5.cc
+++ 백준/Platinum/14003. 가장 긴 증가하는 부분 수열 5/가장 긴 증가하는 부분 수열 5.cc
@@ -1,35 +1,43 @@
-#include<bits/stdc++.h> 
+#include <bits/stdc++.h>
 using namespace std;
-int n,num;
-int lis[1000004],len;
-pair<int,int> ans[1000004];
-stack<int> stk;
-int main() {
-	fill(lis,lis+1000004,1e9+4);
-	cin>>n;
-	for(int i=0; i<n; i++){
-		scanf("%d",&num);
-		auto lowerPos =  lower_bound(lis,lis+len,num);
-		int _pos = (int)(lower_bound(lis,lis+len,num)-lis);
-		if(*lowerPos == 1e9+4) len ++;
-		*lowerPos = num;
-		ans[i].first = _pos;
-		ans[i].second = num;
-	}
-	cout<<len<<'\n';
-	for(int i=n-1; i>= 0; i--){
-		if(ans[i].first == len -1){
-			stk.push(ans[i].second); len--;
-		}
-	}
-	while(stk.size()){
-		cout<<stk.top()<<" ";
-		stk.pop();
-	}
-	return 0;
+int n;
+long long num;
+vector<long long> v2;
+vector<pair<int,long long>> v;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    
+    cin>>n;
+    for(int i=0; i<n; i++){
+        cin>>num;
+        auto it = lower_bound(v2.begin(),v2.end(),num);
+        if(it==v2.end()){
+            v2.push_back(num);
+            v.push_back({v2.size()-1,num});
+        }
+        else{
+            *it = num;
+            v.push_back({it-v2.begin(),num});
+        } 
+    }
+    
+    int len = v2.size();
+    cout<<len<<"\n";
+    stack<long long> st;
+    --len;
+    for(int i=v.size()-1; i>=0; i--){
+        //cout<<"i: "<<i<<" num:"<<v[i].second<<" idx: "<<v[i].first<<"\n";
+        if(v[i].first==len){
+            len--;
+            st.push(v[i].second);
+        }
+    }
+    while(st.size()){
+        cout<<st.top()<<" ";
+        st.pop();
+    }
+    
+    return 0;
 }
-
-
-
-
-
