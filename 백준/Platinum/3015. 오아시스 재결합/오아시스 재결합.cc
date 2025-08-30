cb3242892c52f9f3d0ef0cad57b7afe5ff6f302f
이희수 <hslee9234@naver.com>
2025-08-31 00:15:54 +0900

    [Platinum V] Title: 오아시스 재결합, Time: 68 ms, Memory: 10420 KB -BaekjoonHub

--- 백준/Platinum/3015. 오아시스 재결합/오아시스 재결합.cc
+++ 백준/Platinum/3015. 오아시스 재결합/오아시스 재결합.cc
@@ -1,30 +1,44 @@
-#include<bits/stdc++.h>
-using namespace std;
-typedef long long ll;
-ll n,a;
-stack<pair<ll,ll>> s;
-ll ret;
-int main(){
-	cin>>n;
-	for(int i=0; i<n; i++){
-		cin>>a;
-		int cnt=1;
-		while(!s.empty()&&s.top().first<=a){
-			if(s.top().first==a){
-				cnt=s.top().second+1;
-			}
-			else{
-				cnt =1;
-			}
-			ret+=s.top().second;
-			s.pop();
-		}
-		if(!s.empty()){
-			ret++;
-		}
-		s.push({a,cnt});
-	}
-	cout<<ret<<'\n';
-	return 0;
-}
+/******************************************************************************
 
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <bits/stdc++.h>
+using namespace std;
+long long n,ret;
+long long num;
+vector<long long> v;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    
+    cin>>n;
+    
+    stack<pair<long long, long long>>st;
+    for(int i=0; i<n; i++){
+        cin>>num;
+        int cnt = 1;
+        while(st.size() && st.top().first <= num){
+            if(st.top().first == num){
+                cnt = st.top().second+1;
+            }else{
+                cnt = 1;
+            }
+            ret += st.top().second;
+            st.pop();
+        }
+        if(st.size()){
+            ret++;
+        }
+        st.push({num,cnt});
+    }    
+    cout<<ret;
+
+    
+    return 0;
+}
