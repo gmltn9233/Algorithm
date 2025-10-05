7467ad255c49b1e09893ea21976e6776c69a9160
이희수 <hslee9234@naver.com>
2025-10-05 19:27:50 +0900

    [level 2] Title: 배달, Time: 0.87 ms, Memory: 3.97 MB -BaekjoonHub

--- 프로그래머스/2/12978. 배달/배달.cpp
+++ 프로그래머스/2/12978. 배달/배달.cpp
@@ -1,53 +1,51 @@
 #include <bits/stdc++.h>
+
 using namespace std;
-
 const int INF = 987654321;
-
-// 그래프: route[a] = { {b, w}, ... }
-set<pair<int,int>> route[52];
-int dist[52][52];
-
 priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> pq;
-
+set<pair<int,int>> routes[52];
+int dist[52][52];
 void dijkstra(int start){
     dist[start][start] = 0;
-    pq.push({0, start});
-
-    while(!pq.empty()){
-        auto [cost, from] = pq.top();
+    pq.push({0,start});
+    
+    while(pq.size()){
+        int from = pq.top().second;
+        int weight = pq.top().first;
         pq.pop();
-
-        if(cost > dist[start][from]) continue;
-
-        for(auto it : route[from]){
+        
+        if(dist[start][from] < weight) continue;
+        for(auto it : routes[from]){
             int to = it.first;
-            int weight = it.second;
-
-            if(dist[start][to] > dist[start][from] + weight){
-                dist[start][to] = dist[start][from] + weight;
-                pq.push({dist[start][to], to});
+            int now_weight = it.second;
+            if(dist[start][to] > dist[start][from] + now_weight){
+                dist[start][to] = dist[start][from] + now_weight;
+                pq.push({dist[start][to],to});
             }
         }
     }
 }
-
-int solution(int N, vector<vector<int>> road, int K) {
+int solution(int N, vector<vector<int> > road, int K) {
     int answer = 0;
-    fill(&dist[0][0], &dist[0][0] + 52*52, INF);
-
-    for(auto &it : road){
-        int a = it[0];
-        int b = it[1];
-        int w = it[2];
-        route[a].insert({b, w});
-        route[b].insert({a, w});
+    fill(&dist[0][0], &dist[0][0]+52*52, INF);
+    for(auto it : road){
+        routes[it[0]].insert({it[1],it[2]});
+        routes[it[1]].insert({it[0],it[2]});
     }
-
+    
     dijkstra(1);
-
+    
+    // for(int i=1; i<=N; i++){
+    //     for(int j=1; j<=N; j++){
+    //         cout<<dist[i][j]<<" ";
+    //     }
+    //     cout<<"\n";
+    // }
+    
     for(int i=1; i<=N; i++){
-        if(dist[1][i] <= K) answer++;
+        if(dist[1][i] <= K) ++answer;
     }
-
+    
+    
     return answer;
 }
