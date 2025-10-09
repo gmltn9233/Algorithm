d713564c19c4f9a1c7a4a80a05f4605fbd4d004f
이희수 <hslee9234@naver.com>
2025-10-09 20:42:03 +0900

    [level 3] Title: N으로 표현, Time: 10.46 ms, Memory: 4.34 MB -BaekjoonHub

--- 프로그래머스/3/42895. N으로 표현/N으로 표현.cpp
+++ 프로그래머스/3/42895. N으로 표현/N으로 표현.cpp
@@ -1,29 +1,24 @@
 #include <bits/stdc++.h>
 using namespace std;
-
+set<int>dp[9];
 int solution(int N, int number) {
-    if (N == number) return 1;
-
-    vector<unordered_set<int>> dp(9); // dp[i] : N을 i번 써서 만들 수 있는 수들의 집합
-
-    for (int i = 1; i <= 8; i++) {
-        int repeated = 0;
-        for (int j = 0; j < i; j++) repeated = repeated * 10 + N;
-        dp[i].insert(repeated); // NNN...
-
-        for (int j = 1; j < i; j++) {
-            for (int a : dp[j]) {
-                for (int b : dp[i - j]) {
-                    dp[i].insert(a + b);
-                    dp[i].insert(a - b);
-                    dp[i].insert(a * b);
-                    if (b != 0) dp[i].insert(a / b);
+    for(int i=1; i<=8; i++){
+        int num = 0;
+        for(int j=0; j<i; j++){
+            num = num*10 + N;
+        }
+        dp[i].insert(num);
+        for(int j=1; j<i; j++){
+            for(auto a : dp[j]){
+                for(auto b : dp[i-j]){
+                    dp[i].insert(a+b);
+                    dp[i].insert(a*b);
+                    dp[i].insert(a-b);
+                    if(b!=0) dp[i].insert(a/b);
                 }
             }
         }
-
-        if (dp[i].count(number)) return i;
+        if(dp[i].count(number)) return i;
     }
-
     return -1;
 }
